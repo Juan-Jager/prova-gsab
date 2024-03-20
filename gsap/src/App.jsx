50a6a369f33e2b34b38da97b20adda14f0015409
Juan-Jager <giovanni.dalgrosso@gmail.com>
2024-03-20 18:21:28 +0100

    animazioni finite

--- gsap/src/App.jsx
+++ gsap/src/App.jsx
@@ -1,11 +1,59 @@
-
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap"
 
 const App = () => {
+  const comp = useRef(null);
+
+  useLayoutEffect(()=>{
+
+    let ctx = gsap.context(()=>{
+      const t1 = gsap.timeline()
+      t1.from("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 0.8,
+      }).from(["#title-1", "#title-2"], {
+        opacity: 0,
+        y: "+=30",
+        stagger: 1,
+      }).from(["#title-3", "#title-4"], {
+        opacity: 0,
+        y: "+=30",
+        delay: .5 ,
+        stagger: 2.5,
+      }).to(["#title-4"], {
+        opacity: 0,
+        y: "+=30",
+        delay: .05,
+        stagger: 0.5,
+      }).to(["#title-1", "#title-2", "#title-3"], {
+        opacity: 0,
+        y: "-=30",
+        delay: .5,
+        stagger: 0.5,
+      }).to("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+      }).from("#welcome", {
+        opacity: 0,
+        duration: 1.5,
+      })
+    }, comp)
+    return () => ctx.revert()
+  }, [])
   return (
-    <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+    <div className="relative" ref={comp}>
+      <div  id="intro-slider" className="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col gap-10 justify-center tracking-tighter place-items-center ">
+        <h1 className="text-5xl" id="title-1">Web developer</h1>
+        <h1 className="text-5xl" id="title-2">web design</h1>
+        <h1 className="text-5xl" id="title-3">hacker</h1>
+        <h1 className="text-5xl" id="title-4">cazzo ridi mi scopo tua madre</h1>
+      </div>
+      <div className="h-screen flex bg-gray-950 justify-center place-items-center ">
 
-      <h1 className="text-9xl font-bold text-blue-500">Welcome diocan.</h1>
+        <h1 id="welcome" className="text-9xl font-bold text-gray-100 font-spaceGrotesk">Welcome</h1>
 
+      </div>
     </div>
   )
 }
--- gsap/tailwind.config.js
+++ gsap/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,jsx,css,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
