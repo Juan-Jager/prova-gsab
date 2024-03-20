42ac02667ae36ed1ec9bb800f7dcc366a3a40f47
Juan-Jager <giovanni.dalgrosso@gmail.com>
2024-03-20 23:59:12 +0100

    diocaaa

--- gsap/src/components/Header.jsx
+++ gsap/src/components/Header.jsx
@@ -5,22 +5,25 @@ import gsap from "gsap";
 
 const Header = ()=>{
 
-    useEffect(()=>{
-        gsap.to("#h1",{
-            x: 100,
-            duration:3
-        })
-    },[])
+    // useEffect(()=>{
+    //     gsap.to("#h1",{
+    //         x: 100,
+    //         duration:3
+    //     })
+    // },[])
       
     
 
     return (
+        <div className="flex justify-between items-center bg-gray-950 py-4 px-6">
+        <h1 id="h1" className="text-gray-200">juan</h1>
         <div>
-
-            <h1 id="h1" className="max-w-min">juaan</h1>
-            
-        </div>
+      <a href="/" className="text-gray-200 mr-4">fbi</a>
+      <a href="/" className="text-gray-200">imigracion</a>
+    </div>
+  </div>
     )
 }
 
 export default Header;
+
