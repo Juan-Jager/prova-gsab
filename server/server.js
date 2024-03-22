582fd58bd3dfc77a78a7b44086c82f25393f3573
Juan-Jager <giovanni.dalgrosso@gmail.com>
2024-03-22 23:39:44 +0100

    ora il server 3000 mostra il file in dist

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
-import express from 'express'
-import Main from '../gsap/src/Main.js'
-const app = express()
-const port = 3000
+import express from 'express';
+import  {fileURLToPath}  from 'url';
+import  {dirname}  from 'path';
+import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const app = express();
+const port = 3000;
+
+// Servi i file statici dalla cartella build del tuo progetto React
+app.use(express.static(path.join(__dirname, '../gsap/dist')));
 
 app.get('/', (req, res) => {
-    
-    res.end("<Main/>")
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    // Ritorna il file HTML principale del tuo progetto React
+    res.sendFile(path.join(__dirname, '../gsap/dist', 'index.html'));
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
